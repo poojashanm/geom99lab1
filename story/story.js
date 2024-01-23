74982b634af1eaaf7b4f10eb1fa2a573fbab6370
poojashanm <146376038+poojashanm@users.noreply.github.com>
2024-01-23 18:47:53 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,1 +1,11 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: 13.0827, lng: 80.2707 },
+  });
+  const transitLayer = new google.maps.TransitLayer();
 
+  transitLayer.setMap(map);
+}
+
+window.initMap = initMap;
