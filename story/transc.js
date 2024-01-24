9ecfb117cd32aca8643ff0e8f0c0016b449a453f
poojashanm <146376038+poojashanm@users.noreply.github.com>
2024-01-24 16:23:56 -0500

    Create transc.js

---
+++ story/transc.js
@@ -0,0 +1,12 @@
+let map
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: 13.0827, lng: 80.2707 },
+  });
+  const transitLayer = new google.maps.TransitLayer();
+
+  transitLayer.setMap(map);
+}
+
+window.initMap = initMap;
